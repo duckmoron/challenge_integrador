fe348a0985c9435675ba40843dc62d8f484c23ed
Patricio Morón <patriciomoron@gmail.com>
2023-12-12 21:57:22 -0300

    se actualizó nombre de base de datos y referencias

--- src/models/itemModel.js
+++ src/models/itemModel.js
@@ -2,7 +2,7 @@ const { conn } = require('../config/conn.js');
 
 const getAll = async (params) => {
     try {
-        const [rows] = await conn.query('SELECT product_id, product_name, price, product_description, dues, image_front, image_back, licence_name FROM funko_test.product INNER JOIN funko_test.licence ON funko_test.product.licence_id = funko_test.licence.licence_id LIMIT ?;', params);
+        const [rows] = await conn.query('SELECT product_id, product_name, price, product_description, dues, image_front, image_back, licence_name FROM sql10627826.product INNER JOIN sql10627826.licence ON sql10627826.product.licence_id = sql10627826.licence.licence_id LIMIT ?;', params);
         //console.log(rows);
         return rows;
     } catch (error) {
@@ -16,7 +16,7 @@ const getAll = async (params) => {
 const getById = async (id) => {
     try {
         //console.log("entro al metodo", id);
-        const [item] = await conn.query('SELECT product_id, product_name, price, product_description, dues, image_front, image_back, licence_name FROM funko_test.product INNER JOIN funko_test.licence ON funko_test.product.licence_id = funko_test.licence.licence_id WHERE product_id = ?;', id);
+        const [item] = await conn.query('SELECT product_id, product_name, price, product_description, dues, image_front, image_back, licence_name FROM sql10627826.product INNER JOIN sql10627826.licence ON sql10627826.product.licence_id = sql10627826.licence.licence_id WHERE product_id = ?;', id);
         //console.log("este es el modelo", item);
         return item [0];
     } catch (error) {
@@ -29,7 +29,7 @@ const getById = async (id) => {
 
 const getAll2 = async () => {
     try {
-        const [rows] = await conn.query('SELECT product_id, product_name, sku, licence_name FROM funko_test.product INNER JOIN funko_test.licence ON funko_test.product.licence_id = funko_test.licence.licence_id ORDER BY funko_test.product.product_id;');
+        const [rows] = await conn.query('SELECT product_id, product_name, sku, licence_name FROM sql10627826.product INNER JOIN sql10627826.licence ON sql10627826.product.licence_id = sql10627826.licence.licence_id ORDER BY sql10627826.product.product_id;');
         return rows;
     } catch (error) {
         return "No se pudo obtener la lista de items.";
@@ -40,7 +40,7 @@ const getAll2 = async () => {
 
 const getCollection = async () => {
     try {
-        const [rows] = await conn.query('SELECT licence_id, licence_name, licence_description, licence_image FROM funko_test.licence;');
+        const [rows] = await conn.query('SELECT licence_id, licence_name, licence_description, licence_image FROM sql10627826.licence;');
         return rows;
     } catch (error) {
         return "No se pudo obtener la lista de licence.";
@@ -51,7 +51,7 @@ const getCollection = async () => {
 
 const crearItem = async (categoria, licencia, nombre, descripcion, sku, precio, stock, descuento, cuotas) => {
     try {
-        const [itemNuevo] = await conn.query(`INSERT INTO funko_test.product (product_name, product_description, price, stock, discount, sku, dues, image_front, image_back, licence_id, category_id)
+        const [itemNuevo] = await conn.query(`INSERT INTO sql10627826.product (product_name, product_description, price, stock, discount, sku, dues, image_front, image_back, licence_id, category_id)
         VALUES ("${nombre}", "${descripcion}", "${precio}", "${stock}", "${descuento}", "${sku}", "${cuotas}", "", "", "${licencia}", "${categoria}");`)
 		return itemNuevo;
 	} catch (error) {
@@ -64,7 +64,7 @@ const crearItem = async (categoria, licencia, nombre, descripcion, sku, precio, 
 
 const eliminarItem = async (id) => {
     try {
-        const [itemEliminado] = await conn.query('DELETE FROM funko_test.product WHERE product_id = ?;', [id]);
+        const [itemEliminado] = await conn.query('DELETE FROM sql10627826.product WHERE product_id = ?;', [id]);
         console.log(itemEliminado);
         return itemEliminado;
     } catch (error) {
